d7b9d2773b13d70fefb4a669a72684853f048070
John <johnnkonge2020@gmail.com>
2023-10-26 23:45:59 +0300

    rendered some props but not done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{
+  /* display an <a> tag for each link here */}
+  <a href="#home" key={links}> Home</a>
+  <a href="#about" key={links}>About Me</a>
+  <a href="#projects" key={links}>Projects</a>
+  </nav>;
 }
 
 export default NavBar;
